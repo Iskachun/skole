a3ba75665aff99df247450bfea4d359f25832c0c
Iskachun <141581451+Iskachun@users.noreply.github.com>
2024-11-14 09:56:17 +0000

    dd

--- js/1.js
+++ js/1.js
@@ -1,10 +1,17 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+var mini = true;
+function toggleSidebar() {
+    if (mini) {
+        console.log("opening sidebar");
+        document.getElementById("mySidebar").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+        this.mini = false;
+    } else {
+        console.log("closing sidebar");
+        document.getElementById("mySidebar").style.width = "85px";
+        document.getElementById("main").style.marginLeft = "85px";
+        this.mini = true;
+    }
+}
 
 
   function addTask() { 
